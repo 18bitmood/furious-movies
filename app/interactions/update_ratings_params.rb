95d5c736a7187a0bf4203b86df445bd6f4310d6c
Ivan <ivan.papushoi@mail.ru>
2021-07-05 19:22:38 +0300

    refactor: move update rating params to interactor

---
+++ app/interactions/update_ratings_params.rb
@@ -0,0 +1,17 @@
+class UpdateRatingsParams < ActiveInteraction::Base
+  object :movie
+  
+  # count this params and save it to DB to avoid perfomance issue with many requests
+  def execute
+    movie.average_rating = recount_average_rating
+    movie.ratings_total_number = movie.ratings.count
+    movie.save
+  end
+
+  def recount_average_rating
+    movie_marks = movie.ratings.map(&:mark)
+    return 0 if movie_marks.empty?
+
+    (movie_marks.sum / movie_marks.count.to_f).round(2)
+  end
+end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,25 +4,13 @@ class Rating < ApplicationRecord
 
   validates :mark, presence: true, numericality: {
     only_integer: true,
-    greater_than_or_equal_to: 0,
+    greater_than_or_equal_to: 1,
     less_than_or_equal_to: 10
   }
 
   self.per_page = 10
 
-  
-  # To interactor
   def update_rating_parameters
-    movie.average_rating = recount_average_rating
-    #byebug
-    movie.ratings_total_number = movie.ratings.count
-    movie.save
-  end
-
-  def recount_average_rating
-    movie_marks = movie.ratings.map(&:mark)
-    return 0 if movie_marks.empty?
-
-    (movie_marks.sum / movie_marks.count.to_f).round(2)
+    UpdateRatingsParams.run(movie: movie)
   end
 end
